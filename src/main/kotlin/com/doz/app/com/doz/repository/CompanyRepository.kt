48bf46a9fd117fa9db7c6888a2faf63c9d5ea1e1
Dominik Zimmermann <doz@xetics.de>
2019-05-15 20:09:27 +0200

    Clean up for a basic spring-data feature set for a presentation.

--- src/main/kotlin/com/doz/app/com/doz/repository/CompanyRepository.kt
+++ src/main/kotlin/com/doz/app/com/doz/repository/CompanyRepository.kt
@@ -2,10 +2,7 @@ package com.doz.app.com.doz.repository
 
 import com.doz.app.com.doz.model.Company
 import org.springframework.data.repository.PagingAndSortingRepository
-import org.springframework.data.rest.core.annotation.RepositoryRestResource
 
-@RepositoryRestResource(exported = true)
 interface CompanyRepository : PagingAndSortingRepository<Company, Long> {
-
     fun <T> findAllByNameStartingWith(nameStart: String, type: Class<T>): Collection<T>
 }
