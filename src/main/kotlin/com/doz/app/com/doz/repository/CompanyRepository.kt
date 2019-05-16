e0baf61a80002452758bda38496ebdec51b19712
Dominik Zimmermann <doz@xetics.de>
2019-05-16 09:27:30 +0200

    Chnage to JpaRepository

--- src/main/kotlin/com/doz/app/com/doz/repository/CompanyRepository.kt
+++ src/main/kotlin/com/doz/app/com/doz/repository/CompanyRepository.kt
@@ -1,8 +1,8 @@
 package com.doz.app.com.doz.repository
 
 import com.doz.app.com.doz.model.Company
-import org.springframework.data.repository.PagingAndSortingRepository
+import org.springframework.data.jpa.repository.JpaRepository
 
-interface CompanyRepository : PagingAndSortingRepository<Company, Long> {
+interface CompanyRepository : JpaRepository<Company, Long> {
     fun <T> findAllByNameStartingWith(nameStart: String, type: Class<T>): Collection<T>
 }
--- src/main/kotlin/com/doz/app/com/doz/repository/PersonRepository.kt
+++ src/main/kotlin/com/doz/app/com/doz/repository/PersonRepository.kt
@@ -5,13 +5,13 @@ import com.doz.app.com.doz.model.Person
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Slice
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.data.repository.query.Param
 import org.springframework.transaction.annotation.Transactional
 
 
-interface PersonRepository : PagingAndSortingRepository<Person, Long> {
+interface PersonRepository : JpaRepository<Person, Long> {
 
     @Transactional
     @Query(
